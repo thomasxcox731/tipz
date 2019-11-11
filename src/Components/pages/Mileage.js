a7e9c32a8c6d6e346b2225ffc6e1878493a15742
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-11-11 10:21:19 -0600

    fixed routes

--- src/Components/pages/Mileage.js
+++ src/Components/pages/Mileage.js
@@ -7,10 +7,10 @@ function Mileage() {
       <button type="startTrip" className="btn btn-primary">Start Trip</button>
       <button type="endTrip" className="btn btn-danger">End Trip</button>
       <select class="form-control form-control-lg">
-  <option>Work</option>
-  <option>Medical</option>
-  <option>MOving</option>
-</select>
+        <option>Work</option>
+        <option>Medical</option>
+        <option>MOving</option>
+      </select>
     </div>
   );
 }
