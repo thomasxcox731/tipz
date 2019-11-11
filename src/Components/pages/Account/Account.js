f57bd078ac87028063d6808157126941fa9f18ca
Tom Cox <thomasxcox731@gmail.com>
2019-11-11 12:10:36 -0600

    Added folders for all of the pages js and css files

---
+++ src/Components/pages/Account/Account.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Account.css";
+
+function Account() {
+  return (
+    <div className="container">
+      <h1>Account</h1>
+      <h2>Mileage</h2>
+      <h2>Tips</h2>
+    </div>
+  );
+}
+
+export default Account;
