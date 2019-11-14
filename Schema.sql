c5937984559e4998659824a48842ec6213c1494b
Stephen McMillen <Shtevetm@gmail.com>
2019-11-14 15:35:37 -0600

    added schema and seed for switch to mySQL

---
+++ Schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE user123;
+USE user123;
+
+CREATE TABLE userData (
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE mileageData (
+  id INT NOT NULL AUTO_INCREMENT,
+  mileage DECIMAL (10,3) NOT NULL,
+  category VARCHAR(20) NOT NULL,
+  mileageDate DATE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tipsData (
+  id INT NOT NULL AUTO_INCREMENT,
+  tips  DECIMAL (10,2)  NOT NULL,
+  tipsDate DATE NOT NULL,
+  PRIMARY KEY (id)
+);
